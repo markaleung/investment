3e48776df65931e5224f04cfc4272fec447d36dc
Mark Leung <markaleung@gmail.com>
2024-02-08 21:02:51 +0800

    Save For Retirement - Store start, year, total, annuity, annualised

--- classes/save_for_retirement.py
+++ classes/save_for_retirement.py
@@ -6,27 +6,32 @@ class Single:
         self.mix = mix.copy()
         self.annuity = annuity.Increment()
         self.config = config_ if config_ else config.Config()
-        self.years = []
-    def _run_year_start(self):
+        self.dict = []
+    def _run_start_year(self):
         self.total = 0
-        for year_current, row in enumerate(self.mix.loc[self.year_start:self.year_end].itertuples()):
+        self.return_ = 1
+        for year_current, row in enumerate(self.mix.loc[self.year_start:].itertuples()):
             self.year_current = year_current
             self.row = row
             self.total = (self.total + 1) * row.Return
-            if self.total * row.annuity > 1:
-                self.years.append(self.year_current)
-                break
-    def _run_years_start(self):
+            self.return_ *= row.Return
+            self.dict.append({
+                'start': self.year_start, 
+                'year': self.year_current, 
+                'total': self.total, 
+                'annuity': self.total * row.annuity, 
+                'annualised': self.return_  ** (1/(self.year_current+1)) - 1, 
+            })
+    def _run_start_years(self):
         self.years_length = len(self.mix)-self.config.years_save
         for year_start in self.mix.index[:self.years_length]:
             self.year_start = year_start
-            self.year_end = self.year_start + self.config.years_save
-            self._run_year_start()
-    def _assert_years(self):
-        assert len(self.years) == self.years_length, f'{self.allocation} allocation, {self.years_length} years, {len(self.years)} outputs'
+            self._run_start_year()
+    def _make_df(self):
+        self.df = pd.DataFrame(self.dict)
     def main(self):
-        self._run_years_start()
-        self._assert_years()
+        self._run_start_years()
+        self._make_df()
 
 class Multi:
     def __init__(self, config_ = None):
@@ -39,9 +44,5 @@ class Multi:
             self.tables.make_mix(allocation = allocation)
             self.singles[allocation] = Single(mix = self.tables.mix, config_ = self.config)
             self.singles[allocation].main()
-    def _make_df(self):
-        self.df = pd.DataFrame({allocation: self.singles[allocation].years for allocation in self.config.allocations})
-        self.df.index = self.tables.stock.df.Start[:len(self.tables.stock.df)-self.config.years_save]
     def main(self):
         self._run_singles()
-        self._make_df()
