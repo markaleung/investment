13e0fcdbb07a8f96fa326ad005ea0d2841eea949
Mark Leung <markaleung@gmail.com>
2024-02-09 09:37:21 +0800

    All Years - Fix Bug: add config to tables - Yield Class: join yield with df - Stock and Bond: set allocation, asset dataframe

--- classes/all_years.py
+++ classes/all_years.py
@@ -4,7 +4,7 @@ import pandas as pd
 class AllYears:
     def __init__(self, config_ = None):
         self.config = config_ if config_ else config.Config()
-        self.tables = tables.Tables()
+        self.tables = tables.Tables(config_ = self.config)
         self.dict = []
     def _run_start_year(self):
         self.total = 0
@@ -21,6 +21,7 @@ class AllYears:
                 'year': self.index_current + 1, 
                 'total': self.total, 
                 'total_cash': self.total_cash, 
+                'ratio': self.total / self.total_cash, 
                 'annuity': self.total * row.annuity, 
                 'expected': self.total * row.expected, 
                 'annualised': self.return_ ** (1/(self.index_current+1)) - 1, 
@@ -36,3 +37,23 @@ class AllYears:
         self.tables.make_mix()
         self._run_start_years()
         self._make_df()
+
+class _Yield(AllYears):
+    def _set_subclass_variables(self):
+        pass
+    def _join_yield(self):
+        self.join = pd.merge(self.df[['start', 'year', 'annualised']], self.df_asset[['Start', 'Yield']], left_on = 'start', right_on = 'Start')
+    def main(self):
+        self._set_subclass_variables()
+        super().main()
+        self._join_yield()
+
+class Stock(_Yield):
+    def _set_subclass_variables(self):
+        self.config.allocation = 1
+        self.df_asset = self.tables.stock.df
+
+class Stock(_Yield):
+    def _set_subclass_variables(self):
+        self.config.allocation = 0
+        self.df_asset = self.tables.bond.df
