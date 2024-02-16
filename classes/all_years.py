63ec96c655a8e47ad1cb78e48fdc0b10147ce9db
Mark Leung <markaleung@gmail.com>
2024-02-16 17:36:29 +0800

    Curve Fit - Fit curve - Make prediction - Make residuals - Make sum of squares - Make r squared All Years - Make predictions for ever year - Plot annualised and prediction - Set function for stock and bond

--- classes/all_years.py
+++ classes/all_years.py
@@ -1,4 +1,4 @@
-from classes import tables, config, annuity
+from classes import tables, config, curve_fit
 import pandas as pd
 
 class AllYears:
@@ -43,17 +43,31 @@ class _Yield(AllYears):
         pass
     def _join_yield(self):
         self.join = pd.merge(self.df[['start', 'year', 'annualised']], self.df_asset[['Start', 'Yield']], left_on = 'start', right_on = 'Start')
+    def _make_prediction(self):
+        self.condition = self.join.year == self.year
+        self.filtered = self.join[self.condition]
+        self.curve_fit = curve_fit.CurveFit(x = self.filtered.Yield, y = self.filtered.annualised, function_name = self.function_name)
+        self.curve_fit.main()
+        # .loc is needed so that the prediction is added to self.join, not a copy
+        self.join.loc[self.condition, 'prediction'] = self.curve_fit.prediction
     def main(self):
         self._set_subclass_variables()
         super().main()
         self._join_yield()
+        for self.year in range(1, self.join.year.max()):
+            self._make_prediction()
+    def plot(self, year):
+        self.ax1 = self.join.query('year == @year')[['Yield', 'annualised']].plot(kind = 'scatter', x = 'Yield', y = 'annualised')
+        self.join.query('year == @year')[['Yield', 'prediction']].plot(kind = 'scatter', x = 'Yield', y = 'prediction', ax = self.ax1)
 
 class Stock(_Yield):
     def _set_subclass_variables(self):
         self.config.allocation = 1
         self.df_asset = self.tables.stock.df
+        self.function_name = 'linear'
 
 class Bond(_Yield):
     def _set_subclass_variables(self):
         self.config.allocation = 0
         self.df_asset = self.tables.bond.df
+        self.function_name = 'log'
---
+++ classes/curve_fit.py
@@ -0,0 +1,32 @@
+import numpy as np
+from scipy.optimize import curve_fit
+
+class CurveFit:
+    def __init__(self, x, y, function_name):
+        self.x = x
+        self.y = y
+        self.function_name = function_name
+        self.function = FUNCTIONS[function_name]
+    def _fit_curve(self):
+        (self.a, self.b), self.pcov = curve_fit(self.function, self.x, self.y)
+    def _make_predictions(self):
+        self.prediction = self.function(self.x, self.a, self.b)
+    def _make_residuals(self):
+        self.residuals = self.y - self.prediction
+    def _make_sum_of_squares(self):
+        self.squares_residual = np.sum(self.residuals ** 2)
+        self.squares_total = np.sum((self.y - np.mean(self.y)) ** 2)
+    def _make_r_squared(self):
+        self.r_squared = 1 - (self.squares_residual / self.squares_total)
+    def main(self):
+        self._fit_curve()
+        self._make_predictions()
+        self._make_residuals()
+        self._make_sum_of_squares()
+        self._make_r_squared()
+
+FUNCTIONS = {
+    'log': lambda x, a, b: a * np.log(x) + b, 
+    'inverse': lambda x, a, b: a * (1/x) + b, 
+    'linear': lambda x, a, b: a * x + b, 
+}
